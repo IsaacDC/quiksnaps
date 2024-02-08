69622e3af69f43e65cf467b4335ac81a45b1bed0
sbromfi <131384451+sbromfi@users.noreply.github.com>
2024-02-08 10:58:07 -0500

    Update cameratest.py
    Camera works!

--- cameratest.py
+++ cameratest.py
@@ -1,11 +1,13 @@
 import pygame
-
+import pygame.camera
+import sys
 
 if __name__ == "__main__":
 
     pygame.init()
     display = pygame.display.set_mode((1080,1920), pygame.FULLSCREEN)
-    cam = pygame.camera.Camera(0)
+    pygame.camera.init()
+    cam = pygame.camera.Camera("/dev/video0")
     cam.start()
 
     while True:
@@ -13,22 +15,25 @@ if __name__ == "__main__":
         display.blit(img, (0,0))
         pygame.display.flip()
 
-        for event in events:
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 cam.stop()
                 pygame.quit()
-                return
+                sys.exit()
+
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     print(cam.get_size())
                     cam.stop()
                     pygame.quit()
-                    return
+                    sys.exit()
+
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_s:
                     pygame.image.save(img, "test.png")
                     cam.stop()
                     pygame.quit()
-                    return
+                    sys.exit()
+                    
