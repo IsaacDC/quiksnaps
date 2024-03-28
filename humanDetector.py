75f663b804a9f834a5caf16989a4f4cca4a7586f
Isaac Del Castillo <isaacdc03@hotmail.com>
2024-03-28 16:15:09 -0400

    Upddated Body detector
    Configured to crop anything thats not detected as well as keep the largest item detected

--- humanDetector.py
+++ humanDetector.py
@@ -4,27 +4,33 @@ import numpy as np
 
 pygame.init()
 
-img = cv2.imread('isaac.jpg')
-
-width, height, channels = img.shape
-window = pygame.display.set_mode((height, width))
+img = cv2.imread('IsaacTest.jpg')
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+# upper_body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-bodies, _ = hog.detectMultiScale(gray, winStride=(8, 8), padding=(32, 32), scale=1.05)
+bodies, _ = hog.detectMultiScale(gray, winStride=(8, 8), padding=(32, 32), scale=1.1)
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = np.rot90(img)
-img = pygame.surfarray.make_surface(img)
-window.blit(img, (0, 0))
+sorted_bodies = sorted(bodies, key=lambda x: x[2] * x[3], reverse=True)
 
-for (x, y, w, h) in bodies:
-    pygame.draw.rect(window, (0, 255, 0), (x, y, w, h), 2)
+if len(sorted_bodies) > 0:
+    (x, y, w, h) = sorted_bodies[0]
+    cropped_img = img[y:y+h, x:x+w]
+else:
+    print("No upper bodies detected.")
+    pygame.quit()
+    exit()
 
-pygame.display.update()
+width, height = w, h
+window = pygame.display.set_mode((width, height))
+
+cropped_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
+cropped_img = np.rot90(cropped_img)
+cropped_img = pygame.surfarray.make_surface(cropped_img)
 
 running = True
 while running:
@@ -32,10 +38,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_ESCAPE]:
-        running = False
+        if event.type == pygame.KEYDOWN:
+            running = False
 
+    window.blit(cropped_img, (0, 0))
     pygame.display.update()
 
 pygame.quit()
