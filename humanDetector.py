71e79da0392cbe274c524cba03af6249e24fcdc6
Isaac Del Castillo <isaacdc03@hotmail.com>
2024-04-01 21:53:25 -0400

    Updated humanDetector
    Modified humanDetector.py to look for faces and then crop the image to show the upper half of the body (roughly).
    As well as i added eye detector for testing purposes. Will refine.

--- humanDetector.py
+++ humanDetector.py
@@ -4,33 +4,54 @@ import numpy as np
 
 pygame.init()
 
-img = cv2.imread('IsaacTest.jpg')
+img = cv2.imread('img.png')
 
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-# upper_body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-bodies, _ = hog.detectMultiScale(gray, winStride=(8, 8), padding=(32, 32), scale=1.1)
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(30, 30))
 
-sorted_bodies = sorted(bodies, key=lambda x: x[2] * x[3], reverse=True)
+sorted_faces = sorted(faces, key=lambda x: x[2] * x[3], reverse=True)
 
-if len(sorted_bodies) > 0:
-    (x, y, w, h) = sorted_bodies[0]
-    cropped_img = img[y:y+h, x:x+w]
+if len(sorted_faces) == 1:
+    (x, y, w, h) = sorted_faces[0]
+    top_ratio = 0.5
+    new_width = 4 * w
+    new_height = 5 * h
+    new_height_top = int(top_ratio * new_height)
+    new_x = max(0, x - (new_width - w) // 2)
+    new_y = max(0, y - (new_height_top - h) // 2)
+    img = img[new_y:new_y + new_height, new_x:new_x + new_width]
+
+elif len(sorted_faces) > 1:
+    for (x,y,w,h) in sorted_faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
 else:
-    print("No upper bodies detected.")
+    print("No faces detected. Trying eye detection...")
+
+    eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(20, 20))
+
+    if len(eyes) >= 2:
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+    else:
+        print("No faces or eyes detected.")
+        pygame.quit()
+        exit()
     pygame.quit()
     exit()
 
-width, height = w, h
+width, height = img.shape[1], img.shape[0]
 window = pygame.display.set_mode((width, height))
 
-cropped_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
-cropped_img = np.rot90(cropped_img)
-cropped_img = pygame.surfarray.make_surface(cropped_img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.flip(img, 1)
+img = np.rot90(img)
+img = pygame.surfarray.make_surface(img)
 
 running = True
 while running:
@@ -41,7 +62,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             running = False
 
-    window.blit(cropped_img, (0, 0))
+    window.blit(img, (0, 0))
     pygame.display.update()
 
 pygame.quit()
